d3eca77ce231d9ef52a5985547e6827e1e83ad5b
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2019-11-20 14:31:14 +0100

    ca devrait marcher

--- tp.pl
+++ tp.pl
@@ -1,13 +1,15 @@
-professeur(nom, prenom, discipline, identifiant).
-eleve(nom, prenom, niveau, identifiant).
-salle(nom, capacite, type, identifiant).
-matiere(nom, discipline, type, niveau, identifiant).
-groupe(nom, niveau, liste-eleves, identifiant).
+professeur(nom, prenom, discipline, identifiant)
+eleve(nom, prenom, niveau, identifiant)
+salle(nom, capacite, type, identifiant)
+matiere(nom, discipline, type, niveau, identifiant)
+groupe(nom, niveau, liste-eleves, identifiant)
 seance(matiere, prof, groupe, salle, creneau, identifiant).
 
 add_professeur(nom, prenom, discipline, identifiant):-
     not(professeur(nom, prenom, discipline, identifiant)),
     assert(professeur(nom, prenom, discipline, identifiant)).
+
+
 
 
 
